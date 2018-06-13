b394ca5f2bb085e4eceac856329a359d420a5d8f
Theo Diamantidis <diamaltho@gmail.com>
2018-06-13 12:33:19 +0300

    Moved configuration to separate JSON file

--- config.py
+++ config.py
@@ -1,25 +1,13 @@
 class Config:
-	configs = { \
-		"spring18": { \
-			"folder": "spring18", \
-			"schedule_file": "schedule_jun18.pdf", \
-			"wiki_title": "Εξεταστική_Εαρινή_2018", \
-			"forum_thread": "https://shmmy.ntua.gr/forum/viewtopic.php?f=290&t=22175" \
-		}, \
-		"winter18": { \
-			"folder": "winter18", \
-			"schedule_file": "schedule_jan18.pdf", \
-			"wiki_title": "Εξεταστική_Χειμερινή_2018", \
-			"forum_thread": "https://shmmy.ntua.gr/forum/viewtopic.php?f=290&t=21986" \
-		}, \
-		"sept17": { \
-			"folder": "sept17", \
-			"schedule_file": "schedule_sept17.pdf", \
-			"wiki_title": "Εξεταστική_Επαναληπτική_2017", \
-			"forum_thread": "https://shmmy.ntua.gr/forum/viewtopic.php?f=290&t=21695" \
-		} \
-	}
+	configs = {}
 	ctx = None
+	def __init__(self, configFile = "config.json"):
+		try:
+			with open(configFile, "r", encoding="utf8") as f:
+				import json
+				self.configs = json.load(f)
+		except FileNotFoundError:
+			self.configs = {}
 
 	def context(self, key):
 		if key not in self.configs:
