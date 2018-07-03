2d07f76b2466a104838e357152a9d4a4b8150788
Theo Diamantidis <diamaltho@gmail.com>
2018-07-03 19:15:42 +0300

    Updated README! Courses are now directly fetched from courses.csv; Added user-config.py to .gitignore

--- db/courses.py
+++ db/courses.py
@@ -1,34 +1,27 @@
-import json
+import csv
 from db import keywords
 from pprint import pprint
 from utils import format_text
 
 def get():
-	with open("db/courses.json") as f:
-		return json.load(f)
+	courseList = {}
+	with open("db/courses.csv", newline='') as f:
+		reader = csv.reader(f)
+		for course in reader:
+			id, title, semester, flow, wiki = course
+			courseList[id] = {"title": title, "semester": semester, "flow": flow, "wiki": wiki}
+	return courseList
 
 def print():
 	pprint(get())
 
-def write(j):
-	with open("db/courses.json", "w", encoding="utf8") as fout:
-		fout.write(json.dumps(j))
-
 def update():
-	courseList = {}
-
-	with open("db/course_list.txt") as f:
-		data = f.readlines()
-		for i, course in enumerate(data):
-			course = course.replace("\n", "").split("|")
-			courseList[i] = {"title": course[0], "semester": course[1], "flow": course[2], "wiki": course[3]}
-
+	courseList = get()
 	kw = keywords.get()
 
-	for i, course in courseList.items():
+	for id, course in courseList.items():
 		c = format_text(course["title"])
 		if c not in kw.keys():
-			kw[c] = [int(i)]
+			kw[c] = [int(id)]
 
-	write(courseList)
 	keywords.write(kw)
